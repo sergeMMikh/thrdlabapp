2149e2fe9995818e311809ade59ff76ed75714a3
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-11-02 11:30:51 +0000

    added: news page

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='home'),
+    path('', views.news_home, name='news_home'),
     # path('about', views.about, name='about'),
     # path('contacts', views.contacts, name='contacts'),
     # path('furnaces', views.furnaces, name='furnaces'),
--- news/views.py
+++ news/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def news_home(request):
+    content = {
+        'title': 'News',
+    }
+    return render(request, 'news/news_home.html', content)
