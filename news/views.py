3cefb8395ba06154ccc22c185e1bcf046aa97e60
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-11-04 14:29:41 +0000

    added: view for news

--- news/views.py
+++ news/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Articles
 
 
 def news_home(request):
+    news = Articles.objects.order_by('-date')[:5]
     content = {
         'title': 'News',
+        'news': news,
     }
     return render(request, 'news/news_home.html', content)
