f43ac60577d05967086c84a015c07d5467bb4a79
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-11-21 16:29:50 +0000

    changed: dynamic news page

--- news/urls.py
+++ news/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.news_home, name='news_home'),
     path('create_news', views.create_news, name='create_news'),
-    # path('contacts', views.contacts, name='contacts'),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news-detail'),
     # path('furnaces', views.furnaces, name='furnaces'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
 
 
 def news_home(request):
