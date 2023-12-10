2ffb27687f3057442bec65861249ac636e78c655
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-12-10 16:16:51 +0000

    added: migration001

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-                  # path("admin/", admin.site.urls),
+                  path("admin/", admin.site.urls),
                   path('', include('main.urls')),
                   path('news/', include('news.urls')),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
