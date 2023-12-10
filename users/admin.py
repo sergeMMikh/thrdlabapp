1aefc6b0a848a21012b3efa1db2e376b62ef65d3
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-12-10 16:06:02 +0000

    added: migration001

--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from users.models import User
 
 
-@admin.register(User)
-class ArticlesAdmin(admin.ModelAdmin):
-    list_display = ['username', ]
-    search_fields = ('title', 'date')
+# @admin.register(User)
+# class ArticlesAdmin(admin.ModelAdmin):
+#     list_display = ['username', ]
+#     search_fields = ('title', 'date')
