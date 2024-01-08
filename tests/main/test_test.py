9649bfe61b5e6cb75f6d7db6df8c87b18c891290
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2024-01-08 17:32:02 +0000

    added: selenium test

--- tests/main/test_test.py
+++ tests/main/test_test.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
+
+
 def test_test():
     assert 2 == 2
 
-def test_main_page():
 
+def test_main_page():
     browser = webdriver.Chrome()
 
     browser.get('http://127.0.0.1:8000')
 
     assert 'Home' in browser.title
-
-
