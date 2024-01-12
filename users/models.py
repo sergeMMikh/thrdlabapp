cb24ca784ffa35373728cb5a005a50a14776ccb5
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2024-01-12 16:02:11 +0000

    changed: user model in models.py

--- users/models.py
+++ users/models.py
@@ -6,4 +6,5 @@ class User(AbstractUser):
     telephone_number = models.CharField(max_length=20, null=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.surname}"
+        # return f"{self._meta.fields}"
+        return f"{self.first_name}, {self.last_name}, {self.email}"
